d07ab60abbb6a6708ab6e1669fcf2677cc4e83f1
Ishaan Trivedi <66268900+WizardEclipse@users.noreply.github.com>
2024-05-10 20:53:33 -0700

    Implement initial PAULO side code
    Integration with Adafruit mini OLED
    Displays Flash state with comms through RPi
    Tests UART by implementing PAULO-TESTI handshake

---
+++ IntegrationTests-PAULO/src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET 	-1 // This display does not have a reset pin accessible
+#define BAUD_RATE 9600
+
+Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+volatile int flashFlag = 0;
+String ScreenText;
+
+// Interrupt handler for intial button press
+void handleInterrupt() {
+  if (flashFlag != 1){
+    display_handler.clearDisplay();
+    display_handler.setCursor(0,0);
+    display_handler.println("Flashing...");
+    display_handler.display();
+    flashFlag = 1;
+  }
+  else if (flashFlag == 1)
+  {
+    display_handler.println("Done");
+    display_handler.display();
+    flashFlag = 2;
+  }
+}
+
+void setup() {
+
+  // Sets up mini display I2C
+  display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display_handler.clearDisplay();
+  display_handler.setTextSize(1);
+  display_handler.setTextColor(SSD1306_WHITE);
+  display_handler.setCursor(0,0);
+
+  // Initialize Button Press pin
+  pinMode(PB10, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PB10), handleInterrupt, FALLING);
+
+  while (flashFlag != 2); // waits for flashing to complete
+  
+  Serial1.begin(BAUD_RATE); // Start UART at 9600
+  delay(1000); // Giving testi a lil bit of time
+
+  Serial1.println(101); // Send Handshake From Paulo to Testi
+  display_handler.println("Start UART Test...");
+  display_handler.display();
+
+  delay(3000);
+
+  if (Serial1.available()) {
+    int recievedMessage = Serial1.parseInt(); //read incoming
+    if (recievedMessage == 102) {
+      display_handler.println("Handshake recieved");
+      display_handler.println("UART Success");
+      display_handler.display();
+    }
+    else {
+      display_handler.println("UART Corrupted");
+      display_handler.display();
+    }
+  } else {
+    display_handler.println("UART Timeout");
+    display_handler.display();
+  }
+  
+  
+}
+
+void loop() {};
